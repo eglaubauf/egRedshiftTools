ef0cf649b7fa782e7d7a9cfe7873ebf7191a31c2
elmar <e>
2020-02-19 18:40:25 +0100

    Initial
    broken at the moment

---
+++ scripts/123.py
@@ -0,0 +1,15 @@
+#Set DefaultShape & Color Redshift Lights
+hou.nodeType(hou.objNodeTypeCategory(), "rslight").setDefaultColor(hou.Color(1,1,0))
+hou.nodeType(hou.objNodeTypeCategory(), "rslight").setDefaultShape('light')
+
+hou.nodeType(hou.objNodeTypeCategory(), "rslighties").setDefaultColor(hou.Color(1,1,0))
+hou.nodeType(hou.objNodeTypeCategory(), "rslighties").setDefaultShape('light')
+
+hou.nodeType(hou.objNodeTypeCategory(), "rslightportal").setDefaultColor(hou.Color(1,1,0))
+hou.nodeType(hou.objNodeTypeCategory(), "rslightportal").setDefaultShape('light')
+
+hou.nodeType(hou.objNodeTypeCategory(), "rslightsun").setDefaultColor(hou.Color(1,1,0))
+hou.nodeType(hou.objNodeTypeCategory(), "rslightsun").setDefaultShape('light')
+
+hou.nodeType(hou.objNodeTypeCategory(), "rslightdome::2.0").setDefaultColor(hou.Color(1,1,0))
+hou.nodeType(hou.objNodeTypeCategory(), "rslightdome::2.0").setDefaultShape('light')
